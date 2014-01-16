ded0610b66fb9472a73538536b20c2120c74edc3
OpenShift guest <jakobengdahl@mail.com>
2014-01-16 15:32:39 -0500

    android client folder added

---
+++ AndroidClient/ServerProxy.java
@@ -0,0 +1,5 @@
+// Use RPC to node.js server
+//   JSON-data structure
+// Alternative - connect directly to pubsub
+//   https://github.com/saulpower/Android-Faye-Client
+//   might provide easier integration, might also cause problems connecting through firewall ports
---
+++ AndroidClient/WidgetView.java
@@ -0,0 +1,9 @@
+// Expose WidgetList-element for updating content
+// Expose Menu-regex for filtering
+// Present Widget content for each widget using WebView
+// Allow callbacks to ServerProxy (TriggerEvent)
+// Allow callbacks to WidgetController (ChangeRegex+Rebuild)
+// Allow callbacks to external sources (Android-open: tasker://SomeMobileTask, http://server/sensors/showTemperature/kitchen)
+
+// WebView-events: http://stackoverflow.com/questions/7273025/when-i-invoke-a-java-function-from-my-javascript-code-webview-crashes-why
+// http://www.vogella.com/articles/AndroidListView/article.html
