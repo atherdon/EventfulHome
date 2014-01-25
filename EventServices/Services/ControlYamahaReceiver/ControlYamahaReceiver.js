e49ecc71e2a2ada97373fb362e9b49885a3e0770
OpenShift guest <jakobengdahl@mail.com>
2014-01-25 16:45:56 -0500

    services added

---
+++ EventServices/Services/ControlYamahaReceiver/ControlYamahaReceiver.js
@@ -0,0 +1,25 @@
+// use http-post to control yamaha receiver
+// httppost info: http://openremote.org/display/forums/Controlling++RX+2065+Yamaha+Amp , http://www.theroamingcoder.com/node/111
+//Service listens to watchtvlivingroom, watchmediapclivingroom, playps3livingroom, watchchromecastlivingroom, playmusiclivingroom, allmediaoff
+var config = require('./serviceconfig.json');
+var http = require('http');  
+
+exports.initializeService = function(client) {
+    config.receivermodes.forEach(function(item) { 
+        client.subscribe('/'+item.receivermode, function(message){
+            var httpreq=http.request({
+                host: config.receiverhost,
+                port: config.receiverport,
+                path: config.receiverpath,
+                method: 'POST'
+            }, function(resp) {
+                console.log(resp.statusCode);
+            });
+            item.commands.forEach(function(cmd){
+                console.log(cmd);
+                httpreq.write(cmd);
+            });
+            httpreq.end();
+        });
+    });
+};
