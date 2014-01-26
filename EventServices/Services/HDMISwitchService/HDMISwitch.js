2e90f93729e76ce2173ce4e69295ed62a1092caa
OpenShift guest <jakobengdahl@mail.com>
2014-01-26 18:04:25 -0500

    changed the HDMI switch to use arduino and IR transmitter

--- EventServices/Services/HDMISwitchService/HDMISwitch.js
+++ EventServices/Services/HDMISwitchService/HDMISwitch.js
@@ -2,12 +2,35 @@
 //Service listens to watchtvlivingroom, watchmediapclivingroom, playps3livingroom, watchchromecastlivingroom, watchtvbedroom, watchmediapcbedroom, playps3bedroom, watchchromecastbedroom
 var exec = require('child_process').exec;
 var config = require('./serviceconfig.json');
+var SerialPort = require("serialport").SerialPort;
+var arduinoready=false;
+
+var serialPort;
 
 exports.initializeService = function(client) {
+    serialPort= new SerialPort(config.serialport, {
+      baudrate: config.baudrate,
+      parser: SerialPort.parsers.readline("\n")
+    });
+    
+    serialPort.on("open", function () {
+      console.log('serialport open');
+      arduinoready=true;
+      serialPort.on('close', function(data) {
+        console.log('serialport closed');
+        arduinoready=false;
+      });
+    });
+
     config.HDMISignals.forEach(function(item) {
-        client.subscribe('/'+item.signal, function(message){
-            console.log(config.commandpath+' SEND_ONCE \"'+item.remote+'\" \"'+item.command+'\"');
-            //exec(config.commandpath+' SEND_ONCE \"'+item.remote+'\" \"'+item.signal+'\"', function (error, stdout, stderr) { console.log(stdout) });
+        client.subscribe('/'+item.scene, function(message){
+            console.log(item.signal);
+            if (arduinoready){
+                serialPort.write(item.signal+"\n", function(err, results) {
+                    console.log('err ' + err);
+                    console.log('results ' + results);
+                });   
+            }
         });
     });
 };
