bb3b1902ec8e235670890f3369700827f3a4e3ad
OpenShift guest <jakobengdahl@mail.com>
2014-02-11 07:34:05 -0500

    started working on remote service

--- EventServices/Services/EventfulHomeRemoteService/server.js
+++ EventServices/Services/EventfulHomeRemoteService/server.js
@@ -1,25 +1,42 @@
 //Example from here: http://blog.nodejitsu.com/a-simple-webservice-in-nodejs/
-
-
 var http = require('http'),
     widgets = require('./widgethtml.json'),
     url = require("url");
 
-  var server = http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
     var data = '';
-    var my_path = url.parse(request.url).pathname;  
+    var my_path = url.parse(request.url).pathname; 
+    
     console.log('mypath:'+my_path);
     console.log('Incoming Request', { url: request.url });
-    
+
     request.on('data', function (chunk) {
+      console.log(chunk);
       data += chunk;
     });
-   
-    response.writeHead(501, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(widgets));
-  });
-  
-  server.listen(20000);
+
+    switch(my_path)
+    {
+        case '/getwidgets/':
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(widgets));
+        break;
+        
+        case '/triggerevent/':
+        request.on('end', function(){
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('triggered event');
+            console.log ('complete json:'+data); 
+        });
+        break;
+        
+        default:
+        response.writeHead(501, { 'Content-Type': 'application/json' });
+        response.end('wrong url');
+    }
+    
+});
+server.listen(20000);
   
   
   
