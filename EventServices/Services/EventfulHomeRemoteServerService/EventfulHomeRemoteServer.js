772cc3f1b78bce42372218a9573fd75537801315
OpenShift guest <jakobengdahl@mail.com>
2014-02-12 08:13:23 -0500

    added http and faye functionality to eventfulhomeremoteserver

---
+++ EventServices/Services/EventfulHomeRemoteServerService/EventfulHomeRemoteServer.js
@@ -0,0 +1,88 @@
+//Example from here: http://blog.nodejitsu.com/a-simple-webservice-in-nodejs/
+var http = require('http'),
+    widgets = require('./widgethtml.json'),
+    url = require("url"),
+    config = require('./serviceconfig.json');
+
+var widgetJSONArray=[];
+var resourceJSONArray=[];
+
+exports.initializeService = function(client) {
+    
+    client.subscribe('/addwidget', function(widget){
+        console.log('widget added:'+widget.name);
+        updateOrAppendWidget(widget);
+    });
+    
+    client.subscribe('/addresource', function(resource){
+        console.log('resource added:'+resource.filename);
+        updateOrAppendResource(resource);
+    });
+        
+    var server = http.createServer(function (request, response) {
+        var data = '';
+        var my_path = url.parse(request.url).pathname; 
+        
+        console.log('mypath:'+my_path);
+        console.log('Incoming Request', { url: request.url });
+    
+        request.on('data', function (chunk) {
+          console.log(chunk);
+          data += chunk;
+        });
+    
+        switch(my_path)
+        {
+            case '/getwidgets/':
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(widgets));
+            break;
+            
+            case '/triggerevent/':
+            request.on('end', function(){
+                var jsonEventObj=JSON.parse(data);
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end('triggered event');
+                console.log ('complete json:'+data);
+                client.publish(jsonEventObj.eventname,jsonEventObj.data);
+            });
+            break;
+            
+            case '/addwidget/':
+            request.on('end', function(){
+                var jsonEventObj=JSON.parse(data);
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end('added widget');
+                console.log ('complete json:'+data);
+                updateOrAppendWidget(jsonEventObj);
+            });
+            break;
+            
+            default:
+            response.writeHead(501, { 'Content-Type': 'application/json' });
+            response.end('wrong url');
+        }
+        
+    });
+    server.listen(config.port);
+    
+    client.publish('/widgetrefreshrequested', {includeresources:'true'});
+};
+
+function updateOrAppendWidget(newWidget){
+    for(var i = widgetJSONArray.length - 1; i >= 0; i--) {
+        if(widgetJSONArray[i].name == newWidget.name)
+            widgetJSONArray.splice(i, 1);
+    }
+    widgetJSONArray.push(newWidget);
+}
+
+function updateOrAppendResource(newResource){
+    for(var i = resourceJSONArray.length - 1; i >= 0; i--) {
+        if(resourceJSONArray[i].name == newResource.name)
+            resourceJSONArray.splice(i, 1);
+    }
+    resourceJSONArray.push(newResource);
+}
+    
+  
