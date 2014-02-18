8f1178924afc693619b8e072b7a3bad63202f296
Jakob Engdahl <jakobengdahl@gmail.com>
2014-02-18 20:18:38 +0100

    enabled lirc exec

--- Services/PVRService/PVR.js
+++ Services/PVRService/PVR.js
@@ -6,6 +6,6 @@ var config = require('./serviceconfig.json');
 exports.initializeService = function(client) {
     client.subscribe('/pvrcommandrequested', function(message){
         console.log(config.commandpath+' SEND_ONCE \"'+config.remote+'\" \"'+message.command+'\"');
-        //exec(config.commandpath+' SEND_ONCE \"'+item.remote+'\" \"'+item.signal+'\"', function (error, stdout, stderr) { console.log(stdout) });
+        exec(config.commandpath+' SEND_ONCE \"'+item.remote+'\" \"'+item.signal+'\"', function (error, stdout, stderr) { console.log(stdout) });
     });
 };
