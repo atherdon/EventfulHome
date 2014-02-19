7188eb29de4cdeab45d78d184bbee6001a6294ab
Jakob Engdahl <jakobengdahl@gmail.com>
2014-02-19 23:34:17 +0100

    bugfix

--- Services/PVRService/PVR.js
+++ Services/PVRService/PVR.js
@@ -6,6 +6,7 @@ var config = require('./serviceconfig.json');
 
 exports.initializeService = function(client) {
     client.subscribe('/pvrcommandrequested', function(message){
+    	console.log(message);
         console.log(config.commandpath+' SEND_ONCE \"'+config.remote+'\" \"'+message.signal+'\"');
         exec(config.commandpath+' SEND_ONCE \"'+config.remote+'\" \"'+message.signal+'\"', function (error, stdout, stderr) { console.log(stdout) });
     });
