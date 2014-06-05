58a2487298dc4af58cd2d855ae68b3256c4a664b
Jakob Engdahl <jakobengdahl@gmail.com>
2014-06-05 22:25:10 +0200

    test deployment

--- Services/GitPullService/GitPull.js
+++ Services/GitPullService/GitPull.js
@@ -1,33 +1,9 @@
-//Service listens to lightsonsoft, lightsonbright, lightsoff - all with roomnr as argument
-//should listen to setspecificlight (tellstick-argument-specific)
-//an alternative to using tellstick could be rc-
+//Service listens /gitpullrequested
 var exec = require('child_process').exec;
-
 var config = require('./serviceconfig.json');
-var tdtoolpath = 'tdtool';
-var currentSubscriptions = ['lightsonsoft', 'lightsonbright', 'lightsoff', 'setspecificlight'];
 
 exports.initializeService = function(client) {
-    currentSubscriptions.forEach(function(item) { 
-        client.subscribe('/'+item, function(message){
-            executeLightCommand(item, message);
-        });
+    client.subscribe('/deploymentrequested', function(message){
+        exec(config.gitpullscript, function (error,stdout,stderr) {console.log(stdout);};
     });
 };
-  
-function executeLightCommand(commandtype, message){
-    if (commandtype=='setspecificlight'){
-        //handle specific case
-        console.log("specific lights: id="+message.id+" command="+message.command+" dimlevel="+message.dimlevel);
-    }
-    else{
-        for (var i=0; i<config.roomlights.length; i++){
-            if (message.roomnr == config.roomlights[i].roomnr){
-                exec(tdtoolpath+' '+config.roomlights[i][commandtype], function (error, stdout, stderr) { console.log(stdout) });
-                console.log(tdtoolpath+' '+config.roomlights[i][commandtype]);
-                break;
-            }
-        }
-    }
-}
-
