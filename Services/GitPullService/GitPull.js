821d3335f2e79b61722d97103c3b4570cac62c2c
Jakob Engdahl <jakobengdahl@gmail.com>
2014-06-05 23:14:43 +0200

    test deployment

--- Services/GitPullService/GitPull.js
+++ Services/GitPullService/GitPull.js
@@ -1,23 +1,18 @@
 //Service listens /gitpullrequested
-var spawn = require('child_process').spawn
 var config = require('./serviceconfig.json');
-
+var fs = require('fs'),
+    spawn = require('child_process').spawn;
+ 
 exports.initializeService = function(client) {
     client.subscribe('/deploymentrequested', function(message){
-        var sp=spawn(config.gitpullscript,[], {
-   			detached: true
- 		});
+		var out = fs.openSync('./out.log', 'a'),
+	    	err = fs.openSync('./out.log', 'a');
 
-		sp.stdout.on('data', function (data) {
-		  console.log('stdout: ' + data);
+	 	var child = spawn(config.gitpullscript, [], {
+	   		detached: true,
+	   		stdio: [ 'ignore', out, err ]
 		});
-
-		sp.stderr.on('data', function (data) {
-		  console.log('stderr: ' + data);
-		});
-
-		sp.on('close', function (code) {
-		  console.log('child process exited with code ' + code);
-		});
-    });
+		
+		child.unref();
+	});
 };
