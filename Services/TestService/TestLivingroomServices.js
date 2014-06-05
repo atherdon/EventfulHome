3e93248eb0f7ac97c4f9bdafc84746b55a289cfa
Jakob Engdahl <jakobengdahl@gmail.com>
2014-06-05 22:49:15 +0200

    test deployment

--- Services/TestService/TestLivingroomServices.js
+++ Services/TestService/TestLivingroomServices.js
@@ -1,5 +1,5 @@
 //Service is used to trigger events for test purposes 
 exports.initializeService = function(client) {
     //client.subscribe('/somethingtolistento', function(message){console.log(message);});
-    setTimeout(function(){client.publish('/watchsomething', {roomnr:'3'});}, 4000);
+    setTimeout(function(){client.publish('/watchsomethingelse', {roomnr:'3'});}, 4000);
 };
